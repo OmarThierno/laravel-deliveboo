4ed0259cd363244ae1bf3a55c9ac6225d23ed381
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-07-15 12:18:40 +0200

    Feat: create RestaurantController, index view

---
+++ resources/views/admin/restaurants/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>INDEX RISTORANTE </h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\RestaurantController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,11 +21,13 @@ Route::get('/', function () {
 });
 
 
+
 Route::middleware('auth')
     ->prefix('admin') // Prefisso nell'url delle rotte di questo gruppo
     ->name('admin.') // inizio di ogni nome delle rotte del gruppo
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('restaurants', RestaurantController::class)->parameters(['restaurants' => 'restaurant:slug']);
     });
 
 require __DIR__ . '/auth.php';
