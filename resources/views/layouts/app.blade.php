3cc876dcea50f71b446d73e1f2f2db99bf938f8d
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-07-16 15:23:08 +0200

    Feat: customize home/welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <div class="logo_deliveBoo">
+                    <div class="logo_deliveBoo text-warning">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="50" height="50"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M61.1 224C45 224 32 211 32 194.9c0-1.9 .2-3.7 .6-5.6C37.9 168.3 78.8 32 256 32s218.1 136.3 223.4 157.3c.5 1.9 .6 3.7 .6 5.6c0 16.1-13 29.1-29.1 29.1H61.1zM144 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm240 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32zM272 96a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zM16 304c0-26.5 21.5-48 48-48H448c26.5 0 48 21.5 48 48s-21.5 48-48 48H64c-26.5 0-48-21.5-48-48zm16 96c0-8.8 7.2-16 16-16H464c8.8 0 16 7.2 16 16v16c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V400z"/></svg>
                         <span class="ms-3">DeliveBoo</span>
                     </div>
@@ -41,9 +41,9 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                    <ul class="navbar-nav me-auto ">
+                        <li class="nav-item ">
+                            <a class="nav-link text-danger" href="{{ url('/') }}">{{ __('Home') }}</a>
                         </li>
                     </ul>
 
@@ -52,11 +52,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                                <a class="nav-link text-warning" href="{{ route('login') }}">{{ __('Accedi') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                                    <a class="nav-link text-success" href="{{ route('register') }}">{{ __('Registrati') }}</a>
                                 </li>
                             @endif
                         @else
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
-        <h1 class="display-5 fw-bold">
-            Ti diamo il benvenuto su DeliveBoo.
+<div class="jumbotron background-home p-5 mb-4 bg-light rounded-3">
+    <div class="container welcome py-5">
+        <h1 class="display-5 fw-bold ">
+            Ti diamo il benvenuto su DeliveBoo &hearts;
         </h1>
 
-        <p class="col-md-8 fs-4">In questa sezione potrai accedere al tuo account, oppure registrarti al nostro servizio!</p>
+        <p class="col-md-8 fs-4">In questa sezione potrai accedere al tuo account, oppure registrarti al nostro servizio!
+            <i class="fa-solid fa-utensils"></i>
+        </p>
         <button class="btn btn-success btn-lg" type="button"> <a class="nav-link" href="{{ route('register') }}">{{ __('Inizia subito!') }}</a></button>
     </div>
 </div>
 
 <div class="content">
-    <div class="container">
-        {{-- <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p> --}}
+    <div class="container text-center ">
+        <p class="">Con noi puoi gestire il tuo ristorante</p>
+        <button class="btn btn-warning btn-welcome" type="button"> <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a></button>
     </div>
 </div>
 @endsection
