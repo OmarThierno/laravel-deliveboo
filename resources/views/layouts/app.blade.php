e704839374cf9c2694e1e23df6a2c029680f7192
Denis-Turbatu <turbatudenis81@gmail.com>
2024-07-17 20:00:54 +0200

    feat: added favicon and title to website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="{{ asset('hamburger.svg') }}">
+    <title>DeliveBoo</title>
 
 
     <!-- Fonts -->
