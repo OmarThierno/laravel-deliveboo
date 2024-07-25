3ff7ad809906718b1d4ecf3d54653e75b47ec6a0
OmarThierno <thiernomarndiaye76@gmail.com>
2024-07-25 21:11:53 +0200

    feat: add IMG in restaurant 1

--- config/dishes_restaurant_1.php
+++ config/dishes_restaurant_1.php
@@ -9,7 +9,7 @@ return [
         'allergens' => 'Glutine, Uova, Lattosio',
         'price' => 9.50,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/37/98/2a/37982ab7e39b965ddad554764f68531e.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -19,7 +19,7 @@ return [
         'allergens' => 'Glutine, Lattosio',
         'price' => 12.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/fd/88/4f/fd884f44058fdcfac4692b44c7d3317b.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -29,7 +29,7 @@ return [
         'allergens' => 'Lattosio',
         'price' => 11.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/ea/0d/21/ea0d21cadff4ad0a592debbfeaf1cc32.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -39,7 +39,7 @@ return [
         'allergens' => 'Glutine, Lattosio',
         'price' => 10.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/65/b8/bc/65b8bcbdfa81a4b83176989e6b158bc8.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -49,7 +49,7 @@ return [
         'allergens' => 'Nessuno',
         'price' => 14.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/1e/db/f2/1edbf2849da440b7905ecee39ed14b53.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -59,7 +59,7 @@ return [
         'allergens' => 'Nessuno',
         'price' => 16.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/5e/a7/db/5ea7db690df2680cae531f1e77831062.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -69,7 +69,7 @@ return [
         'allergens' => 'Lattosio',
         'price' => 10.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/7c/38/4c/7c384c43a8e01a40d4dbd005d069a459.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -79,7 +79,7 @@ return [
         'allergens' => 'Pesce, Uova',
         'price' => 13.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/6b/9f/35/6b9f355ca0033fbca9651b8eecf1601d.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -89,7 +89,7 @@ return [
         'allergens' => 'Glutine, Lattosio, Frutta a guscio',
         'price' => 9.50,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/a7/58/bb/a758bb73e279eb2eeaaebd0ce976bf2c.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -99,7 +99,7 @@ return [
         'allergens' => 'Nessuno',
         'price' => 24.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/d9/31/22/d93122977129a90cef26fe0cfc9acbba.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -109,7 +109,7 @@ return [
         'allergens' => 'Lattosio',
         'price' => 8.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/bd/24/09/bd2409d7936eb07315d1c87a51a15d27.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -119,7 +119,7 @@ return [
         'allergens' => 'Pesce, Glutine',
         'price' => 15.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/a5/34/87/a53487f8f7b17b7188f680e99a15d38d.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -129,7 +129,7 @@ return [
         'allergens' => 'Pesce, Glutine',
         'price' => 18.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/4c/d9/51/4cd951915325c699d7aa0bb0776b0663.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -139,7 +139,7 @@ return [
         'allergens' => 'Lattosio, Uova, Glutine',
         'price' => 6.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/aa/b8/76/aab876fedc2b45fb6512b2782ad1506b.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -149,7 +149,7 @@ return [
         'allergens' => 'Lattosio',
         'price' => 5.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/c9/da/c0/c9dac0799a0afa7e15d26040339f225c.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -159,7 +159,7 @@ return [
         'allergens' => 'Glutine, Lattosio',
         'price' => 9.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/c4/23/e7/c423e79f7e842373ca73dbc7080b2d98.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -169,7 +169,7 @@ return [
         'allergens' => 'Glutine',
         'price' => 4.50,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/2e/d0/ff/2ed0ff36b64744d421e1dd9bf528ccc7.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -179,7 +179,7 @@ return [
         'allergens' => 'Glutine, Lattosio',
         'price' => 11.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/3a/f8/9b/3af89b53e0772de0a5b8fb8927c9e090.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -189,7 +189,7 @@ return [
         'allergens' => 'Pesce',
         'price' => 17.00,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/736x/2d/a0/a1/2da0a1852354f2e525e26a31d66cbe1b.jpg'
     ],
     [
         'restaurant_id' => 1,
@@ -199,6 +199,6 @@ return [
         'allergens' => 'Glutine, Frutta a guscio, Lattosio',
         'price' => 10.50,
         'visibility' => true,
-        'thumb' => 'https://via.placeholder.com/150'
+        'thumb' => 'https://i.pinimg.com/474x/d8/24/e3/d824e3494a76ff924a92b856f55dfef3.jpg'
     ]
 ];
