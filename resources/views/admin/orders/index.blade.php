8857aa00f7fb51ac39f830f349bf3b5d69dae096
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-07-26 16:54:41 +0200

    Feat: try to fix table

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -4,40 +4,48 @@
     <div class="container mt-3">
         <h1>Index Orders</h1>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nome Cognome</th>
-                    <th scope="col">Prezzo</th>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col">Stato</th>
-                    <th scope="col">Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($orders as $order)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                     <tr>
-                        <th scope="row">{{ $order->name }} {{ $order->surname }}</th>
-                        <td>{{ $order->price }}</td>
-                        <td>{{ $order->address }}</td>
-                        <td>
-                          <form action="{{route('admin.orders.update', $order->id)}}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <select class="form-select orderSelectStatus" aria-label="Default select example" name="status">
-                              {{-- <option selected>Open this select menu</option> --}}
-                              <option @selected($order->status === 'running') value="running">Incorso</option>
-                              <option @selected($order->status === 'completed') value="completed">Completato</option>
-                            </select>
-                          </form>
-                        </td>
-                        <td scope="col">
-                          <a href="{{route('admin.orders.show', $order->id)}}" class="btn btn-success">Mostra</a>
-                        </td>
-
+                        <th scope="col">Nome Cognome</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Indirizzo</th>
+                        <th scope="col">Stato</th>
+                        <th scope="col">Azioni</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <th scope="row">{{ $order->name }} {{ $order->surname }}</th>
+                            <td>{{ $order->price }}</td>
+                            <td>{{ $order->address }}</td>
+                            <td>
+                                <form action="{{ route('admin.orders.update', $order->id) }}" method="POST">
+                                    @csrf
+                                    @method('PATCH')
+                                    <select class="form-select" aria-label="Default select example" name="status">
+                                        <option @selected($order->status === 'running') value="running">Incorso</option>
+                                        <option @selected($order->status === 'completed') value="completed">Completato</option>
+                                    </select>
+                                </form>
+                            </td>
+                            <td>
+                                <a href="{{ route('admin.orders.show', $order->id) }}"
+                                    class="btn btn-success btn-sm d-none d-md-inline">Mostra</a>
+                                <a href="{{ route('admin.orders.show', $order->id) }}"
+                                    class="btn btn-success btn-sm d-inline d-md-none">Mostra</a>
+                                {{-- _____________________________ --}}
+                                
+                            </td>
+                            <td>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
